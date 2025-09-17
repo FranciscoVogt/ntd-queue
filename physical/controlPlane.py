0551311f15a41ed82e1109fc26bcc4d89f62d5de
FranciscoVogt <franciscogermanovogt@gmail.com>
2025-09-17 21:29:09 +0200

    update

---
+++ physical/controlPlane.py
@@ -0,0 +1,18 @@
+#from netaddr import IPAddress
+p4 = bfrt.physic.pipe
+
+fwd_table = p4.SwitchIngress.fwd
+
+
+fwd_table.add_with_send(ingress_port=164, port=164)
+
+mir = bfrt.mirror
+
+mir.cfg.entry_with_normal(sid = 1, direction = 'BOTH', session_enable = True, ucast_egress_port = 172, ucast_egress_port_valid = 1).push()
+
+
+#multicast configs
+
+
+
+bfrt.complete_operations()
--- physical/run.sh
+++ physical/run.sh
@@ -5,11 +5,11 @@ killall run_switchd
 
 bf_kdrv_mod_load $SDE_INSTALL
 
-/$SDE/../tools/p4_build.sh ntd.p4
+/$SDE/../tools/p4_build.sh physic.p4
 
 
 
-/$SDE/run_switchd.sh -p ntd &
+/$SDE/run_switchd.sh -p physic &
 
 sleep 30
 
