2381167f6ce96a584454a9ce2a5ab2a4adaf1789
FranciscoVogt <franciscogermanovogt@gmail.com>
2025-09-17 22:00:13 +0200

    update

--- physical/controlPlane.py
+++ physical/controlPlane.py
@@ -4,11 +4,11 @@ p4 = bfrt.physic.pipe
 fwd_table = p4.SwitchIngress.fwd
 
 
-fwd_table.add_with_send(ingress_port=164, port=164)
+fwd_table.add_with_send(ingress_port=172, port=172)
 
 mir = bfrt.mirror
 
-mir.cfg.entry_with_normal(sid = 1, direction = 'BOTH', session_enable = True, ucast_egress_port = 172, ucast_egress_port_valid = 1).push()
+mir.cfg.entry_with_normal(sid = 1, direction = 'BOTH', session_enable = True, ucast_egress_port = 164, ucast_egress_port_valid = 1).push()
 
 
 #multicast configs
