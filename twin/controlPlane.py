2c8a650a67f4cfa0f215132d17353b70d6d03571
FranciscoVogt <franciscogermanovogt@gmail.com>
2025-09-18 19:08:42 +0200

    update

--- twin/controlPlane.py
+++ twin/controlPlane.py
@@ -4,7 +4,7 @@ p4 = bfrt.ndt.pipe
 fwd_table = p4.SwitchIngress.fwd
 
 
-fwd_table.add_with_multicast(ingress_port=164, mcast_grp=1)
+fwd_table.add_with_multicast(ingress_port=160, mcast_grp=1)
 
 
 
@@ -13,7 +13,7 @@ fwd_table.add_with_multicast(ingress_port=164, mcast_grp=1)
 
 PRE = bfrt.pre
 
-PRE.node.entry(1, 10, [], [164,172]).push()
+PRE.node.entry(1, 10, [], [144,152]).push()
 
 PRE.mgid.entry(1, [1], [False], [0]).push()
 
