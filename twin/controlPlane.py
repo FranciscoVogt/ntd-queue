e7eef0670348328e3dfd6452e7a9c4dfe3d2d573
FranciscoVogt <franciscogermanovogt@gmail.com>
2025-09-23 01:35:36 +0200

    updt

--- twin/controlPlane.py
+++ twin/controlPlane.py
@@ -9,11 +9,15 @@ fwd_table.add_with_multicast(ingress_port=160, mcast_grp=1)
 
 fwd_table.add_with_send(ingress_port=130, port=130)
 
+fwd_table.add_with_send(ingress_port=52, port=152)
+fwd_table.add_with_send(ingress_port=44, port=144)
+
 #multicast configs
 
 PRE = bfrt.pre
 
-PRE.node.entry(1, 10, [], [144,152]).push()
+#PRE.node.entry(1, 10, [], [144,152]).push()
+PRE.node.entry(1, 10, [], [44,52]).push()
 
 PRE.mgid.entry(1, [1], [False], [0]).push()
 
